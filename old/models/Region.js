ac33c073dadce9e7a0e028983c241781ff16c259
Aleksandr <86127025+Ikiso@users.noreply.github.com>
2022-10-26 17:46:50 +0300

    Delete old directory

--- old/models/Region.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const {Types} = require("mongoose");
-
-const schema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true}
-})
-
-module.exports = mongoose.model('Region', schema)
--- old/models/Year.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-    year: {type: String, required: true, unique: true}
-})
-
-module.exports = mongoose.model('Year', schema)
